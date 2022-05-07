5c1e15f7c984e958aea8917922c4d602ec6c16f4
cescobar-2017353 <cescobar-2017353@kinal.edu.gt>
2022-05-06 23:08:27 -0600

    Vistas del proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EquiposComponent } from "./equipos/equipos.component";
+import { HomeComponent } from "./home/home.component";
+import { LigasComponent } from "./ligas/ligas.component";
+import { LoginComponent } from "./login/login.component";
+import { PerfilComponent } from "./perfil/perfil.component";
+import { RegisterComponent } from "./register/register.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent}, 
+  {path: 'login', component: LoginComponent}, 
+  {path: 'register', component: RegisterComponent},
+  {path: 'ligas', component: LigasComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'equipos', component: EquiposComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LigasComponent } from './ligas/ligas.component';
+import { EquiposComponent } from './equipos/equipos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    LigasComponent,
+    EquiposComponent,
+    PerfilComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
